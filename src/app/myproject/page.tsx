cc63ca791439e522e1fad08eb3fe912f707bdb0c
new 1 <amirhossienus@gmail.com>
2026-07-06 16:18:11 +0330

    add myproject

---
+++ src/app/myproject/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+    Navigation,
+    Pagination,
+    Autoplay,
+    EffectCoverflow,
+} from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-coverflow";
+import "./myproject.css"
+import img1 from "@/public/1.png";
+import img2 from "@/public/2.png";
+import img3 from "@/public/3.png";
+import {brotliCompress} from "node:zlib";
+
+export default function Mypro() {
+    return (
+        <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            loop
+            pagination={{ clickable: true }}
+            autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+            }}
+            modules={[Pagination, Autoplay]}
+            className="projectSwiper"
+        >
+            <SwiperSlide>
+                <div className="card">
+                    <Image src={img1} alt="Project 1" className="project-img" />
+                    <div className="overlay text-danger">
+                        <h3>React</h3>
+                        <p>من در حوضه ی ریکت فعالیت دارم پروژه های متعدد و زیاد</p>
+
+                    </div>
+                </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <div className="card">
+                    <Image src={img2} alt="Project 2" className="project-img" />
+                    <div className="overlay text-danger">
+                        <h3>Next.js</h3>
+                        <p>در حوضه ی نکست هم </p>
+
+                    </div>
+                </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <div className="card">
+                    <Image src={img3} alt="Project 3" className="project-img" />
+                    <div className="overlay text-danger">
+                        <h3>وبلاگ</h3>
+                        <p>React + API</p>
+
+                    </div>
+                </div>
+                <div className="my-5"></div>
+            </SwiperSlide>
+        </Swiper>
+
+    );
+}
